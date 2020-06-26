c39ce5a3d4ff037875d588fdad674870d1c46ba0
baojiong20176 <baojiong20176@hundsun.com>
2020-06-26 13:41:52 +0800

    增加消费者filter(无环境暂时未测试)

---
+++ dubbo-consumer/src/main/java/com/baojiong/dubbo/filter/ConsumerFilter.java
@@ -0,0 +1,13 @@
+package com.baojiong.dubbo.filter;
+
+/**
+ * 源程序名称：ComsumerFilter <br>
+ * 源程序包名：com.baojiong.dubbo.filter <br>
+ * 软件著作权：恒生电子股份有限公司 版权所有 <br>
+ * 系统名称：dubbo-practice <br>
+ * 开发时间：2020/6/26 1:39 下午 <br>
+ *
+ * @author baojiong20176
+ */
+public class ComsumerFilter {
+}
