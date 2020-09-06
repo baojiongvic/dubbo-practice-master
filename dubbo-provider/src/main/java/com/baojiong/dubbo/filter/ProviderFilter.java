560c9ea97775bc057aec023cd32562cafa5fa9a2
baojiong20176 <baojiong20176@hundsun.com>
2020-09-06 22:55:47 +0800

    delete providerFilter copyright

--- dubbo-provider/src/main/java/com/baojiong/dubbo/filter/ProviderFilter.java
+++ dubbo-provider/src/main/java/com/baojiong/dubbo/filter/ProviderFilter.java
@@ -6,12 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * 源程序名称：ProviderFilter <br>
- * 源程序包名：com.baojiong.dubbo.filter <br>
- * 软件著作权：恒生电子股份有限公司 版权所有 <br>
- * 系统名称：dubbo-practice <br>
- * 开发时间：2020/6/26 1:19 下午 <br>
- *
  * @author baojiong20176
  */
 @Activate(group = "provider")
